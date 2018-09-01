a01fa7056451ab9db284d5d6d180dfaaf6d9937c
Juno Julius <yunoyulius@gmail.com>
2018-09-01 14:57:17 +0200

    first half ransomNote

--- ransomNote.js
+++ ransomNote.js
@@ -1,8 +1,18 @@
 const harmlessRansomNote = (noteText, magazineText) => {
+let noteArr = noteText.split(' ')
+let magazineArr = magazineText.split(' ')
+let magazineObj = {}
+
+magazineArr.forEach(word =>{
+if (!magazineObj[word]) magazineObj[word] = 0
+     magazineObj[word]++
+})
+
+console.log(magazineObj);
 
 }
 
-
+harmlessRansomNote('', 'this is all the text')
 
 
 
