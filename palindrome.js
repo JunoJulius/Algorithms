2e64d46ad2860718eb1fc73f223ced7adea2e2ca
Juno Julius <yunoyulius@gmail.com>
2018-09-02 19:24:26 +0200

    Palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,13 @@
-function isPalindrome(string) {
-    string = string.toLowerCase();
-    var charactersArr = string.split('');
-    var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    
-    var lettersArr = [];
-    charactersArr.forEach(char => {
-      if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
-    });
-    
-    return lettersArr.join('') === lettersArr.reverse().join('');
-  }
-   
-  isPalindrome("Madam, I'm Adam");
+const isPalindrome = (string) => {
+string = string.toLowerCase()
+let charactersArr = string.split('')
+let validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+let lettersArr = []
+charactersArr.forEach(char => {
+if(validCharacters.indexOf(char)  > -1) lettersArr.push(char)
+})
+if(lettersArr.join('') === lettersArr.reverse().join('')) return true
+else return false 
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama."))
