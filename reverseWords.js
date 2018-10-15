0f1e07a91402ac643e7b91cbb1b17fb1aebbc316
unknown <yunoyulius@gmail.com>
2018-10-15 13:40:36 +0200

    reverseWords

--- reverseWords.js
+++ reverseWords.js
@@ -1,4 +1,15 @@
 const reverseWords = (string) => {
+  const wordsArray = string.split(' ')
+  const reversedWordArray = []
 
+  wordsArray.forEach(word => {
+    let reversedWord = ''
+    for ( let i = word.length -1; i >= 0 ; i--){
+      reversedWord += word[i]
+    }
+    reversedWordArray.push(reversedWord)
+  })
+  return reversedWordArray.join(' ')
   }
    
+console.log( reverseWords("this is a string of words"));
